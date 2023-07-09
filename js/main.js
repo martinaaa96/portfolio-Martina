b0f63cb9cb0fab3d073c6394ff4428b92afc4055
Martina Vangelova <mvangelovaa@abv.bg>
2023-07-09 17:34:02 +0300

    css

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* ------- about section tabs-------- */
+
+(()=>{
+
+    const aboutSection= document.querySelector(".active-section"),
+    tabsContainer = document.querySelector('.about-tabs');
+
+    tabsContainer.addEventListener('click', (e)=>{
+console.log(e.taget)
+    })
+})();
